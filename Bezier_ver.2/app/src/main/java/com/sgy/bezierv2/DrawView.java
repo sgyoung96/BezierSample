9d98c18a4ca1594e0b733a38e1110d353fb88135
송가영 <sgyoung96@naver.com>
2021-11-22 15:20:11 +0900

    랜덤으로 점 2개 찍기 성공

--- Bezier_ver.2/app/src/main/java/com/sgy/bezierv2/DrawView.java
+++ Bezier_ver.2/app/src/main/java/com/sgy/bezierv2/DrawView.java
@@ -11,34 +11,27 @@ import androidx.annotation.Nullable;
 public class DrawView extends View {
 
     Paint mPaint = new Paint();
+    PathPoint p0;
+    PathPoint p1;
 
-    public DrawView(Context context) {
+    public DrawView(Context context, PathPoint p0, PathPoint p1) {
         super(context);
+        this.p0 = p0;
+        this.p1 = p1;
         setBackgroundColor(getResources().getColor(R.color.transparent));
-    }
-
-    public DrawView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
-
-    public DrawView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
-
-    public DrawView(Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
-        super(context, attrs, defStyleAttr, defStyleRes);
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
-        mPaint.setColor(getResources().getColor(R.color.red));
+        mPaint.setColor(getResources().getColor(R.color.black));
         mPaint.setStrokeWidth(50);
         mPaint.setAntiAlias(true); // 경계면을 부드럽게
         mPaint.setStrokeCap(Paint.Cap.ROUND); // BUTT : 지정한 좌표에서 선이 끝남 ROUND: 둥근 모양으로 끝이 마무리 SQUARE : 사각형 모양이되 지정된 자표보다 조금 더 그어짐
         mPaint.setStrokeJoin(Paint.Join.ROUND); // MITER : 모서리를 90도 각진 형태 (default 값) BEVEL: 모서리가 깎인 형태 ROUND : 둥근 형태
 
-        canvas.drawPoint(50,50, mPaint);
+        canvas.drawPoint((float) p0.x,(float) p0.y, mPaint);
+        canvas.drawPoint((float) p1.x,(float) p1.y, mPaint);
     }
 }
--- Bezier_ver.2/app/src/main/java/com/sgy/bezierv2/MainActivity.java
+++ Bezier_ver.2/app/src/main/java/com/sgy/bezierv2/MainActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.Display;
+import android.view.View;
+import android.view.WindowManager;
 
 import com.sgy.bezierv2.databinding.ActivityMainBinding;
 
@@ -14,6 +17,11 @@ import com.sgy.bezierv2.databinding.ActivityMainBinding;
 public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding = null;
+    PathPoint p0 = new PathPoint();
+    PathPoint p1 = new PathPoint();
+
+    private int mWidth;
+    private int mHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,20 +29,34 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-//        double startP = getRandomPoint();
-//        double endP = getRandomPoint();
-//
-//        Canvas canvas = new Canvas();
-//        Paint mPaint = new Paint();
-//        mPaint.setColor(getResources().getColor(R.color.red));
-//        mPaint.setStrokeWidth(100);
-//        canvas.drawPoint((float) startP, (float) endP, mPaint);
+        binding.btnRedo.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showRandomPoint();
+            }
+        });
 
-        binding.mainCanvas.addView(new DrawView(this));
+        // binding.mainCanvas.addView(new DrawView(this, p0, p1));
 
     }
 
-    private double getRandomPoint() {
-        return Math.random();
+    private void showRandomPoint() {
+        // 화면 사이즈
+        Display display = ((WindowManager)getSystemService(WINDOW_SERVICE)).getDefaultDisplay();
+        mWidth = display.getWidth();
+        mHeight = display.getHeight();
+
+        p0.x = getRandomPoint(0, mWidth);
+        p0.y = getRandomPoint(0, mHeight);
+
+        p1.x = getRandomPoint(0, mWidth);
+        p1.y = getRandomPoint(0, mHeight);
+
+        binding.mainCanvas.addView(new DrawView(this, p0, p1));
+    }
+
+    private double getRandomPoint(double min, double max) {
+        Double randomPoint = (Math.random() * (max - min)) + min;
+        return randomPoint;
     }
 }
