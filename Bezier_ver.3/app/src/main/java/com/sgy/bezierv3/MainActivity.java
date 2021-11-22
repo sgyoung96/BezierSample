8558d04357df74426039141164cfd64e9568f3a5
송가영 <sgyoung96@naver.com>
2021-11-22 17:41:40 +0900

    터치한 곳에 정확하게 좌표 찍기 성공

--- Bezier_ver.3/app/src/main/java/com/sgy/bezierv3/MainActivity.java
+++ Bezier_ver.3/app/src/main/java/com/sgy/bezierv3/MainActivity.java
@@ -3,6 +3,7 @@ package com.sgy.bezierv3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Display;
 import android.view.MotionEvent;
 import android.view.WindowManager;
@@ -15,9 +16,6 @@ public class MainActivity extends AppCompatActivity {
 
     float userTouchX;
     float userTouchY;
-
-    private int mWidth;
-    private int mHeight;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +30,27 @@ public class MainActivity extends AppCompatActivity {
         switch (ev.getAction()) {
             case MotionEvent.ACTION_DOWN: {
                 userTouchX = ev.getX();
-                userTouchY = ev.getY();
+                userTouchY = ev.getY() - (getStatusbarHeight() + getActionBarHeight());
                 binding.mainCanvas.addView(new DrawView(this, userTouchX, userTouchY));
             }
         }
 
         return true;
+    }
+
+    private float getStatusbarHeight() {
+        int resourceId = this.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            return this.getResources().getDimensionPixelOffset(resourceId);
+        } else {
+            return 0;
+        }
+    }
+
+    private float getActionBarHeight() {
+        TypedValue tv = new TypedValue();
+        getTheme().resolveAttribute(R.attr.actionBarSize, tv, true);
+        float actionBarHeight = getResources().getDimensionPixelSize(tv.resourceId);
+        return actionBarHeight;
     }
 }
