955e8c6bae3644de580cc30ab2380e78f81c7ceb
송가영 <sgyoung96@naver.com>
2021-11-23 12:36:45 +0900

    intent 완성

--- Bezier_ver.3/app/src/main/java/com/sgy/bezierv3/activity/Bezier3Activity.java
+++ Bezier_ver.3/app/src/main/java/com/sgy/bezierv3/activity/Bezier3Activity.java
@@ -2,7 +2,9 @@ package com.sgy.bezierv3.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import com.sgy.bezierv3.databinding.ActivityBezier3Binding;
 
@@ -15,5 +17,16 @@ public class Bezier3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityBezier3Binding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        binding.btnNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBezier1 = new Intent(getApplicationContext(), MainActivity.class);
+                goBezier1.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(goBezier1);
+                overridePendingTransition(0, 0);
+                finish();
+            }
+        });
     }
 }
